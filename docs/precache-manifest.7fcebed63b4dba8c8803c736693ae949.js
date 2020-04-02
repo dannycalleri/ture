475f5f2320966b4c62954e0f0caf26e2a85fdcce
Danny Calleri <daniele.calleri@gmail.com>
2020-04-02 12:10:30 +0200

    added docs files

---
+++ docs/precache-manifest.7fcebed63b4dba8c8803c736693ae949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6fce5b54e84d8cb6e616475030b832",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901d615d1298a92eceda",
+    "url": "/static/css/main.a50f9151.chunk.css"
+  },
+  {
+    "revision": "180379223996c0aed6a9",
+    "url": "/static/js/2.65718248.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.65718248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901d615d1298a92eceda",
+    "url": "/static/js/main.b0753f34.chunk.js"
+  },
+  {
+    "revision": "b6468cf6c53ff238f66f",
+    "url": "/static/js/runtime-main.1dde1e0c.js"
+  }
+]);
